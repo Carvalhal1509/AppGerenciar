86003b89f7380efb88f6f97264c404ed3c896afa
Nessa <nessa94.L@hotmail.com>
2024-08-21 21:12:35 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DesafioSenaiCimatec.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
