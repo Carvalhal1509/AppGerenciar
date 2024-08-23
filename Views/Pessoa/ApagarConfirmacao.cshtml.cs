3e90b1ce8081ba84733253cb5b736da97fb3fdea
Nessa <nessa94.L@hotmail.com>
2024-08-22 21:56:07 -0300

    Crud usuarios

---
+++ Views/Pessoa/ApagarConfirmacao.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DesafioSenaiCimatec.Views.Pessoa
+{
+    public class ApagarConfirmacaoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
