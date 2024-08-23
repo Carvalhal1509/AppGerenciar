f88800adc9216f0b54ad58e0a6923bad98efcd3d
Nessa <nessa94.L@hotmail.com>
2024-08-23 00:52:24 -0300

    Usuarios Inativos

--- Controllers/RegistrarController.cs
+++ Controllers/RegistrarController.cs
@@ -59,13 +59,14 @@ namespace DesafioSenaiCimatec.Controllers
             List<TB_USUARIO> aList = query.OrderBy(x => x.NM_PESSOA).Skip(iDisplayStart).Take(iDisplayLength).ToList();
 
             var data = aList.Select(x => new
+
             {
                 ide_usuario = x.ID_USUARIO,
                 nome = x.NM_PESSOA,
                 email = x.DS_EMAIL,
                 cpf = x.NR_CPF_PES,
                 dtc_nascimento = x.DT_NAS_PESSOA.ToString("dd/MM/yyyy"),
-                perfil = x.TP_USUARIO == Enums.TP_USUARIO.Administrador ? "Administrador" : x.TP_USUARIO == Enums.TP_USUARIO.Usuariocadastro ? "Cadastro usuário" : x.TP_USUARIO == Enums.TP_USUARIO.Usuarioconsulta ? "Consulta usuário" : "Padrão",
+                perfil = x.TP_USUARIO == Enums.TP_USUARIO.Administrador ? "Admininistrador" : x.TP_USUARIO == Enums.TP_USUARIO.Usuariocadastro ? "Usuario cadastro" : "Usuario consulta",
                 reativar = $"<a href='#' type='button' class='btn btn-success' onclick='modalReativar({x.ID_USUARIO})'>Reativar</a>",
             }).ToArray();
 
@@ -91,6 +92,7 @@ namespace DesafioSenaiCimatec.Controllers
                 @ViewBag.DS_EMAIL = usuario.DS_EMAIL;
                 @ViewBag.NR_CPF_PES = usuario.NR_CPF_PES;
                 @ViewBag.DT_NAS_PESSOA = usuario.DT_NAS_PESSOA;
+                List<TB_USUARIO> contato = _contatoRepositorio.BuscarTodos();
             }
             return View();
         }
